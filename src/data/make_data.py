0ef40067db8e81431a2638e562618aaa523376d4
tagyera <bibeklimbu036@gmail.com>
2026-04-07 00:32:34 +0545

    initial commit

---
+++ src/data/make_data.py
@@ -0,0 +1,97 @@
+import pandas as pd
+from glob import glob
+
+# --------------------------------------------------------------
+# Read single CSV file
+# --------------------------------------------------------------
+
+single_file_acc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
+
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
+
+single_file_acc.head()
+single_file_gyr.head()
+
+# --------------------------------------------------------------
+# List all data in data/raw/MetaMotion
+# --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/*.csv")
+
+len(files)
+
+# --------------------------------------------------------------
+# Extract features from filename
+# --------------------------------------------------------------
+data_path = "../../data/raw/MetaMotion/"
+
+f = files[0]
+
+participants = f.split("-")[0].replace("\\", "/").replace(data_path, "")
+
+label = f.split("-")[1]
+
+category = f.split("-")[2].rstrip("123")
+
+df = pd.read_csv(f)
+
+df["participants"] = participants
+df["label"] = label
+df["category"] = category
+
+# --------------------------------------------------------------
+# Read all files
+# --------------------------------------------------------------
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+
+for f in files:
+    participants = f.split("-")[0].replace("\\", "/").replace(data_path, "")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+    df = pd.read_csv(f)
+
+    df["participants"] = participants
+    df["label"] = label
+    df["category"] = category
+
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+    if "Gyroscope" in f:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
+
+
+# --------------------------------------------------------------
+# Working with datetimes
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Turn into function
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Merging datasets
+# --------------------------------------------------------------
+
+# --------------------------------------------------------------
+# Resample data (frequency conversion)
+# --------------------------------------------------------------
+
+# Accelerometer:    12.500HZ
+# Gyroscope:        25.000Hz
+
+# --------------------------------------------------------------
+# Export dataset
+# --------------------------------------------------------------
