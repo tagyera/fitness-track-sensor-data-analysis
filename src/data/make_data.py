6ac60f5ac6cabf4709aa68de3eca13276ba1217e
tagyera <bibeklimbu036@gmail.com>
2026-04-07 16:52:36 +0545

    test:processing_raw_data

--- src/data/make_data.py
+++ src/data/make_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from glob import glob
 
+"""
 # --------------------------------------------------------------
 # Read single CSV file
 # --------------------------------------------------------------
@@ -76,14 +77,115 @@ for f in files:
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.info()
+
+pd.to_datetime(df["epoch (ms)"], unit="ms")
+
+
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyr_df["epoch (ms)"]
+del gyr_df["time (01:00)"]
+del gyr_df["elapsed (s)"]
+
+"""
 
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/*.csv")
+data_path = "../../data/raw/MetaMotion/"
+
+
+def read_data_from_files(files):
+
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+
+    acc_set = 1
+    gyr_set = 1
+
+    for f in files:
+        participants = f.split("-")[0].replace("\\", "/").replace(data_path, "")
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+        df = pd.read_csv(f)
+
+        df["participants"] = participants
+        df["label"] = label
+        df["category"] = category
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+        if "Gyroscope" in f:
+            df["set"] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+    del acc_df["epoch (ms)"]
+    del acc_df["time (01:00)"]
+    del acc_df["elapsed (s)"]
+
+    del gyr_df["epoch (ms)"]
+    del gyr_df["time (01:00)"]
+    del gyr_df["elapsed (s)"]
+
+    return acc_df, gyr_df
+
+
+acc_df, gyr_df = read_data_from_files(files)
+
 
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
+data_merged = pd.merge(
+    acc_df.reset_index(),
+    gyr_df.reset_index(),
+    how="outer",
+    on=["epoch (ms)", "participants", "label", "category", "set"],
+)
+
+data_merged.set_index("epoch (ms)", inplace=True)
+
+data_merged = data_merged[
+    [
+        "x-axis (g)",
+        "y-axis (g)",
+        "z-axis (g)",
+        "x-axis (deg/s)",
+        "y-axis (deg/s)",
+        "z-axis (deg/s)",
+        "label",
+        "category",
+        "participants",
+        "set",
+    ]
+]
+
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "label",
+    "category",
+    "participants",
+    "set",
+]
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
@@ -92,6 +194,31 @@ for f in files:
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
+sampling = {
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyr_x": "mean",
+    "gyr_y": "mean",
+    "gyr_z": "mean",
+    "label": "last",
+    "category": "last",
+    "participants": "last",
+    "set": "last",
+}
+
+data_merged[:1000].resample(rule="200ms").apply(sampling)
+
+# split by days
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+data_resampled = pd.concat(
+    [df.resample(rule="200ms").apply(sampling).dropna() for df in days]
+)
+
+data_resampled["set"] = data_resampled["set"].astype("int")
+data_resampled.info()
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
